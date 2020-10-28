57909927843b18511a382c965eec3acaa6bf62c7
Christina <christina.sund@tealium.com>
2020-10-28 16:01:17 -0700

    2.1.1

--- tealium/collectors/attribution/TealiumASIdentifierManager.swift
+++ tealium/collectors/attribution/TealiumASIdentifierManager.swift
@@ -8,9 +8,7 @@
 #if os(iOS) && !targetEnvironment(macCatalyst)
 import AdSupport
 import Foundation
-#if attribution
 import TealiumCore
-#endif
 import UIKit
 #if canImport(AppTrackingTransparency)
 import AppTrackingTransparency
