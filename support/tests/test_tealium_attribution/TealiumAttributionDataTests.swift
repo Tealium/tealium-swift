48133706df9a89248484b407580a6fc14e306b13
christinasund <christina.sund@tealium.com>
2021-02-26 10:11:25 -0800

    "wip"

--- support/tests/test_tealium_attribution/TealiumAttributionDataTests.swift
+++ support/tests/test_tealium_attribution/TealiumAttributionDataTests.swift
@@ -196,17 +196,17 @@ class TealiumAttributionDataTests: XCTestCase {
         XCTAssertEqual("restricted", trackingAuthStatus, "Tracking Authorization Status was an unexpected value")
     }
 
-//    @available(iOS 14, *)
-//    func testTrackingEnabled_ReturnsTrue_WhenTrackingAuthorized() {
-//        let expect = expectation(description: "testTrackingEnabled_ReturnsTrue_WhenTrackingAuthorized")
-//        var idManager = TealiumASIdentifierManager.shared
-//        idManager.attManager = MockATTrackingManagerTrackingAuthorized()
-//        TestTealiumHelper.delay(for: 2.0, on: DispatchQueue(label: expect.description)) {
-//            XCTAssertEqual(idManager.isAdvertisingTrackingEnabled, "true")
-//            expect.fulfill()
-//        }
-//        wait(for: [expect], timeout: 2.2)
-//    }
+    @available(iOS 14, *)
+    func testTrackingEnabled_ReturnsTrue_WhenTrackingAuthorized() {
+        let expect = expectation(description: "testTrackingEnabled_ReturnsTrue_WhenTrackingAuthorized")
+        var idManager = TealiumASIdentifierManager.shared
+        idManager.attManager = MockATTrackingManagerTrackingAuthorized()
+        TestTealiumHelper.delay(for: 2.0, on: DispatchQueue(label: Thread.threadName)) {
+            XCTAssertEqual(idManager.isAdvertisingTrackingEnabled, "true")
+            expect.fulfill()
+        }
+        wait(for: [expect], timeout: 2.2)
+    }
 
     @available(iOS 14, *)
     func testTrackingEnabled_ReturnsFalse_WhenTrackingDenied() {
@@ -244,6 +244,20 @@ class TealiumAttributionDataTests: XCTestCase {
         wait(for: [expect], timeout: 1.2)
     }
 
+}
+
+extension Thread {
+
+    static var threadName: String {
+        if let currentOperationQueue = OperationQueue.current?.name {
+            return "\(currentOperationQueue)"
+        } else if let underlyingDispatchQueue = OperationQueue.current?.underlyingQueue?.label {
+            return "\(underlyingDispatchQueue)"
+        } else {
+            let name = __dispatch_queue_get_label(nil)
+            return String(cString: name, encoding: .utf8) ?? Thread.current.description
+        }
+    }
 }
 
 public class TealiumASIdentifierManagerAdTrackingEnabled: TealiumASIdentifierManagerProtocol {
