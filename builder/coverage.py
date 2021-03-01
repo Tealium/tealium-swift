e36c747157111fccde288d0a4e15ae512ffcfca4
christinasund <christina.sund@tealium.com>
2021-03-01 14:16:29 -0800

    "wip"

---
+++ builder/coverage.py
@@ -0,0 +1,20 @@
+import sys
+import urllib.request
+import json
+from decimal import Decimal
+
+url = str(sys.argv[1])
+req = urllib.request.Request(url)
+res = urllib.request.urlopen(req).read()
+content = json.loads(res.decode('utf-8'))
+
+if len(content):
+	latest = content['commits'][0]
+	if 'totals' in latest:
+		totals = latest['totals']
+		if 'c' in totals:
+			coverage = totals['c']
+			if Decimal(coverage) > 65.0:
+				sys.exit(0)
+			else:
+				sys.exit(1)
