b098ac6f3cb50aecb4026ff062ecba84c76929e3
christinasund <christina.sund@tealium.com>
2021-03-02 14:00:18 -0800

    "wip"

--- builder/coverage.py
+++ builder/coverage.py
@@ -1,21 +1,14 @@
 import sys
-import urllib.request
-import json
+import re
+filename = str(sys.argv[1])
 from decimal import Decimal
-
-url = str(sys.argv[1])
-req = urllib.request.Request(url)
-res = urllib.request.urlopen(req).read()
-content = json.loads(res.decode('utf-8'))
-
-if len(content):
-	latest = content['commits'][0]
-	if 'totals' in latest:
-		totals = latest['totals']
-		if 'c' in totals:
-			coverage = totals['c']
-			print("Coverage: ", coverage)
-			if Decimal(coverage) > 65.0:
-				sys.exit(0)
-			else:
-				sys.exit(1)
+with open(filename) as f:
+	lines = f.readlines()
+	line = lines[2]
+	print(line)
+	coverage = re.findall('\d*\.?\d+',line)[2]
+	print(coverage)
+	if Decimal(coverage) > 65.0:
+		sys.exit(0)
+	else:
+		sys.exit(1)
