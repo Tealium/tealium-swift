216905b4cca4abcb1dd4ae9c569737a679ede887
christinasund <christina.sund@tealium.com>
2021-03-15 11:40:31 -0700

    wip

--- .github/secrets/decrypt_certs.sh
+++ .github/secrets/decrypt_certs.sh
@@ -1,5 +1,5 @@
 echo -n "$CERTIFICATES_P12" | base64 --decode --output cert.p12
-echo -n "$CERTIFICATES_PROFILE" | base64 --decode --output profile.mobileprovision
+echo -n "$CERTIFICATES_PROFILE" | base64 --decode --output Tealium_Automation.mobileprovision
 KEYCHAIN_PATH=$RUNNER_TEMP/login.keychain
 security create-keychain -p $KEYCHAIN_PASSWORD $KEYCHAIN_PATH
 security set-keychain-settings -lut 21600 $KEYCHAIN_PATH
@@ -8,4 +8,4 @@ security import cert.p12 -P $CERTIFICATES_P12_PASSWORD -A -t cert -f pkcs12 -k $
 security list-keychain -d user -s $KEYCHAIN_PATH
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
-cp profile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles
+cp Tealium_Automation.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles
