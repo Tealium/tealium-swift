150ef968c3ad3b478a0f6ed826f0627bb7c873b6
craigrouse <craig.rouse@gmail.com>
2021-04-09 12:00:08 +0100

    comments

--- tealium/dispatchers/tagmanagement/TagManagementWKWebView.swift
+++ tealium/dispatchers/tagmanagement/TagManagementWKWebView.swift
@@ -23,7 +23,6 @@ enum InternalWebViewState: Int {
 class TagManagementWKWebView: NSObject, TagManagementProtocol {
 
     var webview: WKWebView?
-//    var webviewConfig: WKWebViewConfiguration?
     var tealConfig: TealiumConfig
     var webviewDidFinishLoading = false
     var enableCompletion: ((_ success: Bool, _ error: Error?) -> Void)?
