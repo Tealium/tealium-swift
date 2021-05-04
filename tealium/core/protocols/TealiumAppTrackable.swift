29d7c1d890dae1da0bed50102aef2e349568c1d0
craigrouse <craig.rouse@gmail.com>
2021-05-04 14:37:50 +0100

    wip

--- tealium/core/protocols/TealiumAppTrackable.swift
+++ tealium/core/protocols/TealiumAppTrackable.swift
@@ -20,12 +20,25 @@ public struct TealiumAppTrackable<Content: View>: View {
 
     public var body: some View {
         content
+        // handles all standard deep links and universal links
         .onOpenURL(perform: { url in
-            let notification = Notification(name: Notification.Name(rawValue: TealiumValue.deepLinkNotificationName),
-                                            object: nil,
-                                            userInfo: [TealiumKey.deepLinkURL: url])
-            NotificationCenter.default.post(notification)
+            postNotification(url: url)
         })
+        // For some reason, if the link is initiated from camera/NFC tag, this is called and onOpenURL is not called 🤷‍♂️
+        // https://stackoverflow.com/questions/65150897/swiftui-universal-links-not-working-for-nfc
+        .onContinueUserActivity(NSUserActivityTypeBrowsingWeb, perform: { activity in
+            guard let url = activity.webpageURL else {
+                return
+            }
+            postNotification(url: url)
+        })
+    }
+    
+    private func postNotification(url: URL) {
+        let notification = Notification(name: Notification.Name(rawValue: TealiumValue.deepLinkNotificationName),
+                                        object: nil,
+                                        userInfo: [TealiumKey.deepLinkURL: url])
+        NotificationCenter.default.post(notification)
     }
 }
 #endif
