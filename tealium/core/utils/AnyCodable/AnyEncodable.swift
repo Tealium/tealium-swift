a4573c222449e6a8e0f801d852a449a01b393a70
Enrico Zannini <enricozannini93@gmail.com>
2021-08-20 17:29:19 +0200

    Improve AnyEncodable handling and reduce it's complexity
    Fix a crash on Double.greatestFiniteMagnitude and NSNumbers (created with that value) encoding

--- tealium/core/utils/AnyCodable/AnyEncodable.swift
+++ tealium/core/utils/AnyCodable/AnyEncodable.swift
@@ -48,6 +48,12 @@ import Foundation
  let json = try! encoder.encode(dictionary)
  */
 
+extension Encodable {
+  fileprivate func encode(to container: inout SingleValueEncodingContainer) throws {
+    try container.encode(self)
+  }
+}
+
 public struct AnyEncodable: Encodable {
     public let value: Any
 
@@ -68,58 +74,18 @@ extension AnyEncodable: _AnyEncodable {}
 extension _AnyEncodable {
     public func encode(to encoder: Encoder) throws {
         var container = encoder.singleValueContainer()
-
+        if let enc = value as? Encodable {
+            try enc.encode(to: &container)
+            return;
+        }
+        
         switch value {
-        #if os(macOS) || os(iOS) || os(watchOS) || os(tvOS)
         case let number as NSNumber:
-            if Double(truncating: number).isNaN {
-                try container.encode("NaN")
-            } else if Double(truncating: number) == Double.infinity {
-                try container.encode("Infinity")
-            } else {
-                try encode(nsnumber: number, into: &container)
-            }
-        #endif
+            // We could also add Encodable protocol conformance to NSNumber and therefore enter in the previous if case
+            // But it would be public and would interfere with any other third party implementation of that same protocol
+            try encode(nsnumber: number, into: &container)
         case is NSNull, is Void:
             try container.encodeNil()
-        case let bool as Bool:
-            try container.encode(bool)
-        case let int as Int:
-            try container.encode(int)
-        case let int8 as Int8:
-            try container.encode(int8)
-        case let int16 as Int16:
-            try container.encode(int16)
-        case let int32 as Int32:
-            try container.encode(int32)
-        case let int64 as Int64:
-            try container.encode(int64)
-        case let uint as UInt:
-            try container.encode(uint)
-        case let uint8 as UInt8:
-            try container.encode(uint8)
-        case let uint16 as UInt16:
-            try container.encode(uint16)
-        case let uint32 as UInt32:
-            try container.encode(uint32)
-        case let uint64 as UInt64:
-            try container.encode(uint64)
-        case let float as Float:
-            try container.encode(float)
-        case let double as Double:
-            if double.isNaN {
-                try container.encode("NaN")
-            } else if double == Double.infinity {
-                try container.encode("Infinity")
-            } else {
-                try container.encode(double)
-            }
-        case let string as String:
-            try container.encode(string)
-        case let date as Date:
-            try container.encode(date)
-        case let url as URL:
-            try container.encode(url)
         case let array as [Any?]:
             try container.encode(array.map {
                 $0 as? AnyCodable ?? AnyCodable($0)
@@ -134,45 +100,53 @@ extension _AnyEncodable {
         }
     }
 
-    #if os(macOS) || os(iOS) || os(watchOS) || os(tvOS)
     private func encode(nsnumber: NSNumber, into container: inout SingleValueEncodingContainer) throws {
+        let encodable: Encodable
         switch CFNumberGetType(nsnumber) {
         case .charType:
-            try container.encode(nsnumber.boolValue)
+            encodable = nsnumber.boolValue
         case .sInt8Type:
-            try container.encode(nsnumber.int8Value)
+            encodable = nsnumber.int8Value
         case .sInt16Type:
-            try container.encode(nsnumber.int16Value)
+            encodable = nsnumber.int16Value
         case .sInt32Type:
-            try container.encode(nsnumber.int32Value)
+            encodable = nsnumber.int32Value
         case .sInt64Type:
-            try container.encode(nsnumber.int64Value)
+            encodable = nsnumber.int64Value
         case .shortType:
-            try container.encode(nsnumber.uint16Value)
+            encodable = nsnumber.uint16Value
         case .longType:
-            try container.encode(nsnumber.uint32Value)
+            encodable = nsnumber.uint32Value
         case .longLongType:
-            try container.encode(nsnumber.uint64Value)
+            encodable = nsnumber.uint64Value
         case .intType, .nsIntegerType, .cfIndexType:
-            try container.encode(nsnumber.intValue)
+            encodable = nsnumber.intValue
         case .floatType, .float32Type:
-            try container.encode(nsnumber.floatValue)
+            encodable = nsnumber.floatValue
         case .doubleType, .float64Type, .cgFloatType:
-            if nsnumber == NSDecimalNumber.notANumber {
-                try container.encode("NaN")
-            } else if Double(truncating: nsnumber) == Double.infinity {
-                try container.encode("Infinity")
+            let d = Double(truncating: nsnumber)
+            /**
+             * Double.greatestFiniteMagnitude when passed via NSNumber to decimalValue becomes nan. But actually is still a valid Double.
+             * NSNumber(Double.greatestFiniteMagnitude).decimalValue -> NaN -> NOT Encodable
+             *
+             * Using Double conversion of truncating nsnumber (from which decimalValue is nan) still resolves in that valid Double
+             * Double(truncating: NSNumber(Double.greatestFiniteMagnitude)) -> Double.greatestFiniteMagnitude -> Encodable
+             *
+             * We use then Double.nan, Double.infinity or, eventally Double.greatestFiniteMagnitude, by just using the Double(truncating: nsnumber)
+             */
+            if nsnumber == NSDecimalNumber.notANumber || nsnumber.decimalValue.isNaN || d == Double.infinity {
+                encodable = d
             } else {
-                try container.encode(nsnumber.decimalValue)
+                encodable = nsnumber.decimalValue
             }
         #if swift(>=5.0)
         @unknown default:
             let context = EncodingError.Context(codingPath: container.codingPath, debugDescription: "AnyCodable value cannot be encoded")
-            throw EncodingError.invalidValue(value, context)
+            throw EncodingError.invalidValue(nsnumber, context)
         #endif
         }
+        try encodable.encode(to: &container)
     }
-    #endif
 }
 
 extension AnyEncodable: Equatable {
