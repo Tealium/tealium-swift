9ea49135b1fe433aca107088d56413f400ed42fa
Enrico Zannini <enricozannini93@gmail.com>
2021-09-03 13:31:18 +0200

    Add secureMainThreadExecution method and use it where needed
    Avoid main thread sync on disable webview to avoid any unnecessary possible risk of deadlocks
    Remove completion for sync attachToUIView and setRootView methods
    Add tests for disable/deinit TagManagementWKWebView

--- tealium/dispatchers/tagmanagement/TagManagementProtocol.swift
+++ tealium/dispatchers/tagmanagement/TagManagementProtocol.swift
@@ -77,9 +77,8 @@ protocol TagManagementProtocol {
     ///￼
     /// - Parameters:
     ///     - view: `UIView` instance for WKWebView to be attached to
-    ///     - completion: Completion block to be run when the operation has completed
-    func setRootView(_ view: UIView,
-                     completion: ((_ success: Bool) -> Void)?)
+    /// - returns: a success `Bool`, true if the webview was successfully attached
+    func setRootView(_ view: UIView) -> Bool
 
 }
 #endif
