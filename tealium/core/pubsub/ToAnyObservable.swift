50858853c3d17cb259762638d2c97d467c01adc1
Enrico Zannini <enricozannini93@gmail.com>
2021-09-03 18:10:33 +0200

    Improve rename and reorganize pubsub classes

---
+++ tealium/core/pubsub/ToAnyObservable.swift
@@ -0,0 +1,26 @@
+//
+//  ToAnyObservable.swift
+//  TealiumCore
+//
+//  Created by Enrico Zannini on 03/09/21.
+//  Copyright © 2021 Tealium, Inc. All rights reserved.
+//
+
+import Foundation
+
+@propertyWrapper
+public final class ToAnyObservable<P: TealiumPublisherProtocol>: TealiumPublisherProtocol {
+    
+    private let publisher: P
+    public init(_ anyPublisher: P) {
+        self.publisher = anyPublisher
+    }
+    
+    public var wrappedValue: TealiumObservable<P.Element> {
+        return asObservable()
+    }
+    
+    public func asObservable() -> TealiumObservable<P.Element> {
+        return publisher.asObservable()
+    }
+}
