c84d20c18bdd4891dcf2c1864c8264dabf097e96
Enrico Zannini <enricozannini93@gmail.com>
2021-09-15 15:15:22 +0200

    [WIP] Try to add watchOS app extension
    WatchOS app extension can't import tealium frameworks

---
+++ builder/AutoTrackingWatchOSTest Extension/tealium_swiftApp.swift
@@ -0,0 +1,20 @@
+//
+//  tealium_swiftApp.swift
+//  AutoTrackingWatchOSTest Extension
+//
+//  Created by Enrico Zannini on 15/09/21.
+//  Copyright © 2021 Tealium, Inc. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct tealium_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
