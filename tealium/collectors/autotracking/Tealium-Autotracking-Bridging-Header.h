c7ccab641f533e9e725c78ad773f9a8dc49c27c7
Enrico Zannini <enricozannini93@gmail.com>
2021-11-02 10:39:24 +0100

    Remove unused bridging header

--- tealium/collectors/autotracking/Tealium-Autotracking-Bridging-Header.h
+++
@@ -1,6 +0,0 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
-// Alternatively add the following imports to a preexisting bridging header
-#import "UIViewController+TealiumTracker.h"
